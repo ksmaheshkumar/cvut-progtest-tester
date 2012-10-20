1290dedc096bdee8e81c7b9e926103c2b7351524
Jan Žák <zj@zakjan.cz>
2012-10-20 03:11:06 +0200

    init

---
+++ progtest.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Progtest tester
+# @author Jan Žák <zj@zakjan.cz>
+
+app_file="main" # application executable file
+data_dir="data" # path to test data
+
+echo "  Progtest tester"
+echo "  $app_file"
+echo "-----------------------------------"
+for input in `ls $data_dir/*_in.txt`; do
+	output="out.txt"
+	output_ref="${input/in/out}"
+
+	echo -n "> Test $input: "
+	./$app_file < $input > $output
+	log=`diff $output $output_ref`
+
+	if [ "$log" == "" ]; then
+		echo "OK"
+	else
+		echo "CHYBA"
+		echo
+		echo "> vstup:"
+		cat $input
+		echo
+		echo "> výstup:"
+		cat $output
+		echo
+		echo "> referenční výstup:"
+		cat $output_ref
+		echo
+	fi
+
+	rm out.txt
+done
