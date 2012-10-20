e60b008b1cb673ae2f9f88142936f5a5d37ea174
Jan Žák <zj@zakjan.cz>
2012-10-20 03:41:05 +0200

    parametrizovane, okomentovane

--- progtest.sh
+++ progtest.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
 # Progtest tester
+# Can be used with downloadable sample data from progtest
+# - files with names: *_(in|out).txt
+#
 # @author Jan Žák <zj@zakjan.cz>
 # @license MIT
 
-app_file="main" # application executable file
-data_dir="data" # path to test data
+app_file=${1:-"main"} # application executable file
+data_dir=${2:-"data"} # path to test data
+
+
 
 echo "  Progtest tester"
 echo "  $app_file"
 echo "-----------------------------------"
+
+
+
+# for each sample input file
 for input in `ls $data_dir/*_in.txt`; do
 	output="out.txt"
-	output_ref="${input/in/out}"
+	output_ref=${input/in/out}
 
 	echo -n "> Test $input: "
+
+	# run!
 	./$app_file < $input > $output
+
+	# compare current and sample output
 	log=`diff $output $output_ref`
 
 	if [ "$log" == "" ]; then
