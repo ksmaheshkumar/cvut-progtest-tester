1638d9879130ee9bcfaa496129849aed7f6a24bd
Jan Žák <zj@zakjan.cz>
2012-10-20 03:55:17 +0200

    komentare

--- progtest.sh
+++ progtest.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
 # Progtest tester
-# Can be used with downloadable sample data from progtest
-# - files with names: *_(in|out).txt
+# - executes your application multiple times against sample input data and
+# compares its output with correct output
+# - used sample data files should be named *_(in|out).txt
+#
+# Usage example:
+# ./progtest.sh [application_filename [directory_with_test_data]]
 #
 # @author Jan Žák <zj@zakjan.cz>
 # @license MIT
 
-app_file=${1:-"main"} # application executable file
-data_dir=${2:-"data"} # path to test data
-
-
-
-echo "  Progtest tester"
-echo "  $app_file"
-echo "-----------------------------------"
+app_file=${1:-"main"}
+data_dir=${2:-"data"}
 
 
 
 # for each sample input file
 for input in `ls $data_dir/*_in.txt`; do
 	output="out.txt"
-	output_ref=${input/in/out}
+	output_ref=${input/_in/_out}
 
 	echo -n "> Test $input: "
 
